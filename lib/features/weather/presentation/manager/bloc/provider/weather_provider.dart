4738c93c678fce51ce71e07e9a601bc538cd64f4
Ignacio Previgliano <ignaprevi@gmail.com>
2024-08-08 19:19:44 -0300

    commit mvp

--- lib/features/weather/presentation/manager/bloc/provider/weather_provider.dart
+++ lib/features/weather/presentation/manager/bloc/provider/weather_provider.dart
@@ -6,13 +6,16 @@ import 'package:weather_app/core/services/geo_locator_service.dart';
 class GeoLocatorProvider extends ChangeNotifier {
   final GeoLocatorService _geoLocatorService = GeoLocatorService();
   Position? _currentPosition;
-
   double? _latitude;
   double? _longitude;
+  bool _isLoading = false;
+  String? _errorMessage;
 
   Position? get currentPosition => _currentPosition;
   double? get latitude => _latitude;
   double? get longitude => _longitude;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
 
   void startLocationUpdates() {
     _geoLocatorService.startListeningToLocationUpdates((Position position) {
@@ -28,11 +31,22 @@ class GeoLocatorProvider extends ChangeNotifier {
   }
 
   Future<void> getCurrentLocation() async {
-    _currentPosition = await _geoLocatorService.determinePosition();
-    if (_currentPosition != null) {
-      _latitude = _currentPosition!.latitude;
-      _longitude = _currentPosition!.longitude;
-    }
+    _isLoading = true;
     notifyListeners();
+    try {
+      _currentPosition = await _geoLocatorService.determinePosition();
+      if (_currentPosition != null) {
+        _latitude = _currentPosition!.latitude;
+        _longitude = _currentPosition!.longitude;
+        _errorMessage = null;
+      } else {
+        _errorMessage = 'No se pudo obtener la ubicación.';
+      }
+    } catch (e) {
+      _errorMessage = 'Error al obtener la ubicación: $e';
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
   }
 }
